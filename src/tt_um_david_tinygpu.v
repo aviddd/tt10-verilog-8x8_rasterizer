658794577dbfd212f7efcd7b629715b9076742ec
aviddd <72466852+aviddd@users.noreply.github.com>
2024-12-13 17:40:08 +0530

    Update tt_um_david_tinygpu.v

--- src/tt_um_david_tinygpu.v
+++ src/tt_um_david_tinygpu.v
@@ -6,19 +6,19 @@ module tt_um_david_tinygpu (
     input  wire [7:0] uio_in,   // IOs: Input path (unused)
     output wire [7:0] uio_out,  // IOs: Output path (unused)
     output wire [7:0] uio_oe,   // IOs: Enable path (unused)
-    input  wire       ena,      // always 1 when the design is powered
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - active low reset
+    input  wire        ena,     // always 1 when the design is powered
+    input  wire        clk,     // clock
+    input  wire        rst_n    // reset_n - active low reset
 );
 
     // Signals between command_processor and rasterizer
     wire [1:0] out_cmd;
     wire [2:0] out_x1, out_y1, out_x2, out_y2, out_width, out_height;
-    wire       cmd_ready;
+    wire        cmd_ready;
 
     // Rasterizer outputs
     wire [3:0] pixel_data;
-    wire       frame_sync;
+    wire        frame_sync;
 
     // Instantiate the Command Processor
     command_processor cmd_proc (
