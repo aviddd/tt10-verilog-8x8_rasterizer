90b5a6c9848463c446eaf60ada1cd9511fafd005
aviddd <72466852+aviddd@users.noreply.github.com>
2024-12-15 00:02:01 +0530

    Update graphics_processor.v

--- src/graphics_processor.v
+++ src/graphics_processor.v
@@ -10,6 +10,9 @@ module graphics_processor (
 
     reg [7:0] frame_buffer [0:7];
     reg [5:0] pixel_count;
+    reg [1:0] latched_command; // Added latched command
+    reg [2:0] latched_x1, latched_y1, latched_x2, latched_y2, latched_rect_width, latched_rect_height; // Added latched parameters
+
 
     // State machine for graphics processing
     localparam IDLE = 2'd0,
@@ -33,29 +36,37 @@ module graphics_processor (
                 IDLE: begin
                     frame_start <= 1'b0;
                     if (command_valid) begin
+                        // Latch the command and parameters
+                        latched_command <= command;
+                        latched_x1 <= x1;
+                        latched_y1 <= y1;
+                        latched_x2 <= x2;
+                        latched_y2 <= y2;
+                        latched_rect_width <= rect_width;
+                        latched_rect_height <= rect_height;
                         state <= DRAW;
                     end
                 end
                 DRAW: begin
-                    case (command)
+                    case (latched_command) // Use latched command
                         2'b01: begin // DRAW_PIXEL or CLEAR
-                            if (x1 == 3'b111) begin // CLEAR
+                            if (latched_x1 == 3'b111) begin // CLEAR
                                 for (int i = 0; i < 8; i++) begin
                                     frame_buffer[i] <= 8'b00000000;
                                 end
                             end else begin
-                                frame_buffer[y1][x1] <= 1'b1;
+                                frame_buffer[latched_y1][latched_x1] <= 1'b1;
                             end
                         end
                         2'b10: begin // DRAW_LINE
                             // TODO: Implement Bresenham's line algorithm
-                            frame_buffer[y1][x1] <= 1'b1;
-                            frame_buffer[y2][x2] <= 1'b1;
+                            frame_buffer[latched_y1][latched_x1] <= 1'b1;
+                            frame_buffer[latched_y2][latched_x2] <= 1'b1;
                         end
                         2'b11: begin // FILL_RECT
                             // TODO: Implement rectangle fill algorithm
-                            for (int i = y1; i <= y1 + rect_height; i++) begin
-                                for (int j = x1; j <= x1 + rect_width; j++) begin
+                            for (int i = latched_y1; i <= latched_y1 + latched_rect_height; i++) begin
+                                for (int j = latched_x1; j <= latched_x1 + latched_rect_width; j++) begin
                                     if (i < 8 && j < 8) begin
                                         frame_buffer[i][j] <= 1'b1;
                                     end
